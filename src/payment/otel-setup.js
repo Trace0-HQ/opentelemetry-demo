65a1ea6b3a2cd32e10225dc012cf84e9bb8e25ee
davidcervi <dave@autocruit.io>
2026-09-04 03:28:00 +0000

    Disabled log sending in Pino

---
+++ src/payment/otel-setup.js
@@ -0,0 +1,14 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+
+const sdk = new NodeSDK({
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-pino': {
+        disableLogSending: true,
+      },
+    }),
+  ],
+});
+
+sdk.start();
